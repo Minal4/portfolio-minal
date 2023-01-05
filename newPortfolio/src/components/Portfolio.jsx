11d854220b8c940d5822a37aea0b16c1ec34a15a
minal4 <minaltott@gmail.com>
2023-01-05 16:04:22 +0545

    update portfolio

--- newPortfolio/src/components/Portfolio.jsx
+++ newPortfolio/src/components/Portfolio.jsx
@@ -23,80 +23,80 @@ export default function Portfolio() {
         <div className="portfolio__content">
           <ul>
             <li>
-              <a target="_blank" href="https://catchthemes.com/demo/abletone-dark/">
+              <a target="_blank" rel='noopener noreferrer' href="https://catchthemes.com/demo/abletone-dark/">
                 <img src={Abletone} alt="Dark" />
                 <div className="caption">Abletone</div>
               </a>
               <p></p>
             </li>
             <li>
-              <a target="_blank" href="https://catchthemes.com/demo/simclick-pro/">
+              <a target="_blank" rel='noopener noreferrer' href="https://catchthemes.com/demo/simclick-pro/">
                 <img src={SimClick} alt="Dark" />
                 <div className="caption">Simclick</div>
               </a>
             </li>
             <li>
-              <a target="_blank" href="https://catchthemes.com/demo/corporate-biz-pro/">
+              <a target="_blank" rel='noopener noreferrer' href="https://catchthemes.com/demo/corporate-biz-pro/">
                 <img src={Biz} alt="Dark" />
                 <div className="caption">Corporate Biz</div>
               </a>
             </li>
             <li>
-              <a target="_blank" href="https://catchthemes.com/demo/catch-sketch-blog/">
+              <a target="_blank" rel='noopener noreferrer' href="https://catchthemes.com/demo/catch-sketch-blog/">
                 <img src={Blog} alt="Dark" />
                 <div className="caption">Catch Sketch Blog</div>
               </a>
             </li>
             <li>
-              <a target="_blank" href="https://catchthemes.com/demo/catch-sketch-pro/">
+              <a target="_blank" rel='noopener noreferrer' href="https://catchthemes.com/demo/catch-sketch-pro/">
                 <img src={Main} alt="Dark" />
                 <div className="caption">Catch Sketch</div>
               </a>
             </li>
             <li>
-              <a target="_blank" href="http://fgtelecom.com.au/">
+              <a target="_blank" rel='noopener noreferrer' href="http://fgtelecom.com.au/">
                 <img src={FgTelecom} alt="Dark" />
                 <div className="caption">Fg Telecom</div>
               </a>
             </li>
             <li>
-              <a target="_blank" href="https://civilhomes.com.np/">
+              <a target="_blank" rel='noopener noreferrer' href="https://civilhomes.com.np/">
                 <img src={Civil} alt="Dark" />
                 <div className="caption">Civil Homes</div>
               </a>
             </li>
             <li>
-              <a target="_blank" href="https://9ky.bcf.myftpupload.com/">
+              <a target="_blank" rel='noopener noreferrer' href="https://9ky.bcf.myftpupload.com/">
                 <img src={Texas} alt="Dark" />
                 <div className="caption">Texas</div>
               </a>
             </li>
             <li>
-              <a target="_blank" href="https://twesupply.com/">
+              <a target="_blank" rel='noopener noreferrer' href="https://twesupply.com/">
                 <img src={Twe} alt="Dark" />
                 <div className="caption">TWE Supply</div>
               </a>
             </li>
             <li>
-              <a target="_blank" href="https://bodylogic.physio/">
+              <a target="_blank" rel='noopener noreferrer' href="https://bodylogic.physio/">
                 <img src={BodyLogic} alt="Dark" />
                 <div className="caption">BodyLogic</div>
               </a>
             </li>
             <li>
-              <a target="_blank" href="https://themepalace.com/theme-demo/?demo=Mk56ZGY5TVJhVlg0enkwNWRkaW9hUT09">
+              <a target="_blank" rel='noopener noreferrer' href="https://themepalace.com/theme-demo/?demo=Mk56ZGY5TVJhVlg0enkwNWRkaW9hUT09">
                 <img src={SignifyPro} alt="Dark" />
                 <div className="caption">SignifyPro</div>
               </a>
             </li>
             <li>
-              <a target="_blank" href="https://themepalace.com/theme-demo/?demo=Mk56ZGY5TVJhVlg0enkwNWRkaW9hUT09">
+              <a target="_blank" rel='noopener noreferrer' href="https://themepalace.com/theme-demo/?demo=Mk56ZGY5TVJhVlg0enkwNWRkaW9hUT09">
                 <img src={wedding} alt="Dark" />
                 <div className="caption">SignifyPro Wedding</div>
               </a>
             </li>
             <li>
-              <a target="_blank" href="https://themepalace.com/theme-demo/?demo=Mk56ZGY5TVJhVlg0enkwNWRkaW9hUT09">
+              <a target="_blank" rel='noopener noreferrer' href="https://themepalace.com/theme-demo/?demo=Mk56ZGY5TVJhVlg0enkwNWRkaW9hUT09">
                 <img src={ecommerce} alt="Dark" />
                 <div className="caption">SignifyPro Ecommerce</div>
               </a>
