3d45feeb5693bcc39b86b38a1bdb57deca236f1f
minal4 <minaltott@gmail.com>
2023-03-04 16:11:50 +0545

    update ecommerce file

--- src/pages/eCommerce/Product.jsx
+++ src/pages/eCommerce/Product.jsx
@@ -9,14 +9,14 @@ const Product = ({ data, cartHandler }) => {
                 <div className="shop__image">
                     <img src={data.image} alt={data.title} />
                 </div>
-                <div className="shop__content text-center">
-                    <span className='cat py-2 px-5 rounded-3xl text-white text-xs mb-3'>{data.category}</span>
-                    <h2 className='my-3'>{data.title}</h2>
-                    <span className='rating'>{data.rating.rate}</span><br />
-                    <span className='price'>Rs {data.price}</span>
-                    <button onClick={() => cartHandler(data)} className='btn__link'>Add to cart</button>
-                </div>
             </Link>
+            <div className="shop__content text-center">
+                <span className='cat py-2 px-5 rounded-3xl text-white text-xs mb-3'>{data.category}</span>
+                <h2 className='my-3'>{data.title}</h2>
+                <span className='rating'>{data.rating.rate}</span><br />
+                <span className='price'>Rs {data.price}</span>
+                <button onClick={() => cartHandler(data)} className='btn__link'>Add to cart</button>
+            </div>
         </>
     )
 }
