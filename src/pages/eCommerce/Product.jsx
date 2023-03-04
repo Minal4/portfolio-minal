2be8e26985accc5c0f306747f752039408d3af51
minal4 <minaltott@gmail.com>
2023-03-04 16:43:50 +0545

    update ecommerce file

--- src/pages/eCommerce/Product.jsx
+++ src/pages/eCommerce/Product.jsx
@@ -4,12 +4,12 @@ import { Link } from 'react-router-dom'
 const Product = ({ data, cartHandler }) => {
 
     return (
-        <>
-            <Link to={`/singleProduct/${data.id}`}>
-                <div className="shop__image">
+        <div>
+            <div className="shop__image">
+                <Link to={`/singleProduct/${data.id}`}>
                     <img src={data.image} alt={data.title} />
-                </div>
-            </Link>
+                </Link>
+            </div>
             <div className="shop__content text-center">
                 <span className='cat py-2 px-5 rounded-3xl text-white text-xs mb-3'>{data.category}</span>
                 <h2 className='my-3'>{data.title}</h2>
@@ -17,7 +17,7 @@ const Product = ({ data, cartHandler }) => {
                 <span className='price'>Rs {data.price}</span>
                 <button onClick={() => cartHandler(data)} className='btn__link'>Add to cart</button>
             </div>
-        </>
+        </div>
     )
 }
 
